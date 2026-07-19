43d0dbfca5108dd5a07a6c76952e8a1ab8c51e1c
Keyur Asodariya <asodariyakeyur111@gmail.com>
2026-07-19 15:49:34 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length){
+            return false;
+        }
+        char[] c1 = new char[26];
+        char[] c2 = new char[26];
+
+        for(int i=0; i<s.Length; i++){
+            c1[s[i]-'a']++;
+            c2[t[i]-'a']++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(c1[i] != c2[i])
+                return false;
+        }
+
+
+        // char[] c = new char[26];
+
+        // for(int i=0; i<s.Length; i++){
+        //     c[s[i]-'a']++;
+        //     c[t[i]-'a']--;
+        // }
+
+        // for(int i=0; i<26; i++){
+        //     if(c[i] != 0)
+        //         return false;
+        // }
+
+
+        return true;
+    }
+}
