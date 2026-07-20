13f2871f80fa8d19946add8e6a0057d3798a2954
Keyur Asodariya <asodariyakeyur111@gmail.com>
2026-07-20 21:23:40 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public string Encode(IList<string> strs) {
+        StringBuilder sb = new StringBuilder();
+        foreach (var str in strs) {
+            sb.Append(str.Length);
+            sb.Append('#');
+            sb.Append(str);
+        }
+
+        return sb.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        List<string> result = new();
+        int i = 0;
+        while (i < s.Length) {
+            StringBuilder numString = new StringBuilder();
+            while (s[i] != '#') {
+                numString.Append(s[i]);
+                i++;
+            }
+            i++;
+
+            int length = int.Parse(numString.ToString());           
+            result.Add(s.Substring(i, length));
+            i+= length;
+        }
+
+        return result;
+    }
+}
