d747b8982c14ed5605bcccaaa530ab1f853aeb59
Keyur Asodariya <asodariyakeyur111@gmail.com>
2026-07-29 08:43:39 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public bool CheckInclusion(string s1, string s2) {
+        if (s1.Length > s2.Length) {
+            return false;
+        }
+
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+
+        foreach (char c in s1) {
+            cnt1[c - 'a']++;
+        }
+        int matches = 0;
+        for(int i=0; i<26; i++){
+            if(cnt1[i]==0)
+                matches++;
+        }
+
+        int left = 0, right = 0;
+         
+        while (right < s2.Length) {
+            int inChar = s2[right] - 'a';
+            cnt2[inChar]++;
+
+            if((right-left+1) > s1.Length){                
+                int outChar = s2[left] - 'a';
+                if(cnt1[outChar]==cnt2[outChar])
+                    matches--;
+
+                cnt2[outChar]--;
+                left++;
+            }
+
+            if (cnt1[inChar] == cnt2[inChar]){
+                matches++;
+            }
+
+            if(matches == 26)
+                return true;
+            
+            right++;
+        }
+
+        return false;
+    }
+}
