6c53df071ebadc379a66deb139e2aa075eda235c
Keyur Asodariya <asodariyakeyur111@gmail.com>
2026-08-08 10:40:18 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cs
@@ -0,0 +1,35 @@
+public class TimeMap {
+    Dictionary<string, List<(string Value, int Timestamp)>> timeMap;
+    public TimeMap() {
+        timeMap = new();
+    }
+
+    public void Set(string key, string value, int timestamp) {
+        if (!timeMap.ContainsKey(key)) {
+            timeMap[key] = new List<(string Value, int Timestamp)>();
+        }
+
+        timeMap[key].Add((value, timestamp));
+    }
+
+    public string Get(string key, int timestamp) {
+        if (!timeMap.ContainsKey(key))
+            return "";
+
+        var valueList = timeMap[key];
+        int n = valueList.Count;
+        int left = 0, right = n - 1;
+        string value = "";
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (valueList[mid].Timestamp <= timestamp) {
+                value = valueList[mid].Value;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return value;
+    }
+}
