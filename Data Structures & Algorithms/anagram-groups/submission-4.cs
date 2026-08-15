2b34d3ad3198192690b2bfcdf1ae78679e4b2a68
Keyur Asodariya <asodariyakeyur111@gmail.com>
2026-08-15 11:54:49 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> anagramMap = new();
+
+        foreach(string str in strs){
+            string hashKey = GetHashKey(str);
+            if(!anagramMap.TryGetValue(hashKey, out List<string> group)){
+                group = new List<string>();
+            }
+
+            group.Add(str);
+            anagramMap[hashKey] = group;
+        }
+
+        return anagramMap.Values.ToList();
+    }
+
+    private string GetHashKey(string str){
+        int[] charCount = new int[26];
+        foreach(char c in str){
+            charCount[c-'a']++;
+        }
+
+        StringBuilder sb = new();
+
+        for(int i=0; i<26; i++){
+            sb.Append('#');
+            sb.Append(charCount[i]);
+        }
+
+
+        return sb.ToString();
+    }
+}
